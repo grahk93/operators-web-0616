e969782ec4055582b825002291362ed8c58eee5b
grahk93 <grace.juster@flatironschool.com>
2016-06-01 15:59:33 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
+#DID THIS IN THE INEFFICIENT WAY. NEED CLARIFICATION ON IF/ELSE
+
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    true
+  else
+    false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+   speed < 40 || speed > 60 ? true : false
 end
-	
+
 
 
